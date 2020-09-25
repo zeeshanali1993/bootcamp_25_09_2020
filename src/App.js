0512ff014eab4929cd016cfcbaf5a347fd89b4ff
Zeeshan Ali <zeeshanali93@outlook.com>
2020-09-25 20:06:41 +0500

    uploaded

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Counter } from './counterHandler.js';
 
-function App() {
+export default function App()
+{
+  let [count, setCount] = useState(0);
+  let [isMorning, setMorning] = useState(true);
+  // function incrementFunction()
+  // {
+  //   setCount(count + 1);
+  // }
+  // function decrementFunction()
+  // {
+  //   setCount(count - 1);
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={isMorning ? 'box' : 'dayLight'}>
+      {/* <h1>{count}</h1> */}
+      <Counter counter={count} />
+      <button onClick={() => setCount(count + 1)}>Increment +1</button>
+      <br />
+      <button onClick={() => setCount(count - 1)}>Decrement -1</button>
+      <button onClick={() => setMorning(!isMorning)}>Toggle Daylight</button>
     </div>
   );
 }
-
-export default App;
---
+++ src/counterHandler.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function Counter(props)
+{
+    return (
+        <div>
+            <h1>Current value is: {props.counter}</h1>
+        </div>
+    );
+}
+
+//export default Dish;
